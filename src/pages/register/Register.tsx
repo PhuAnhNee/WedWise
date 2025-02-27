92ff49deca8df403eda2711820acd81717c385fb
Mika383 <haitddse172641@fpt.edu.vn>
2025-02-27 23:46:16 +0700

    fix vercel

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -26,7 +26,7 @@ const Register = () => {
         email,
         password,
         avatarUrl,
-        role: 2, // Role mặc định là 1 (user)
+        role: "2", // Chuyển số thành chuỗi
       };
 
       const response = await AuthService.register(user);
